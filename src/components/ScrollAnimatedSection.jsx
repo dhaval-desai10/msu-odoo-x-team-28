eebe82949f1c645f96884520d7f12e0123864cc6
dhaval-desai10 <dhavaldesai7000@gmail.com>
2025-03-07 16:37:09 +0530

    Delete src directory

--- src/components/ScrollAnimatedSection.jsx
+++
@@ -1,9 +0,0 @@
-const ScrollAnimatedSection = ({ children, className = "" }) => {
-  return (
-    <div className={`py-20 ${className}`}>
-      {children}
-    </div>
-  );
-};
-
-export default ScrollAnimatedSection; 
